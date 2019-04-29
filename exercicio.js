f0b5396b9ae152bad9acd2cbfbd7092fc2dfdb51
Luiz Fernando <psnluiz@hotmail.com>
2019-04-29 16:27:11 -0300

    adicionando script js com refatoramento

---
+++ exercicio.js
@@ -0,0 +1,63 @@
+var dias = ["domingo","segunda feira", "terça feira", "quarta feira", "quinta feira", "sexta feira", "sabado"];
+
+
+var alternar = function (s){
+    var chars = s.toLowerCase().split("");
+    for(var i=0; i < chars.length; i+=2){
+        chars[i] = chars[i].toUpperCase();
+    }
+    return chars.join("");
+}
+
+for (var i=0; i<dias.length; i++){
+    // console.log(dias[i]);
+    dias[i] = alternar(dias[i]);
+    // console.log(dias[i]);
+}
+
+
+// var buscarIndice = function(arg){ //refatorar usando find
+//     for (var i=0; i<dias.length; i++){
+//         if ( dias[i].toLowerCase().includes(arg.toLowerCase()) ){
+//             console.log("O indíce do dia " + dias[i] + " é : " + (i+1));
+//         }
+//     }
+// }
+
+//refatoração usando find
+var buscarIndice = function(arg){
+    dias.find(function(element, index){
+        if(element.toLowerCase().includes(arg.toLowerCase())){console.log("O indíce do dia " + element + " é :" + (index+1))} 
+    });
+} 
+
+
+
+
+// var diasComS = function(){ 
+//     for(var i=0; i<dias.length; i++){
+//         if(dias[i].toLowerCase().charAt(0) == 's'){
+//             console.log(dias[i]);
+//         }
+//     }
+// }
+
+//refatoração usando FILTER
+var diasComS = dias.filter(dia => dia.toLowerCase().charAt(0) == 's');
+
+
+//execução 
+console.log("\n Busca de Índice")
+buscarIndice("qua");
+
+console.log("\n\n Dias com S: " + diasComS);
+
+
+// for(var i=0; i<dias.length; i++){
+//     buscarIndice(dias[i]);
+// }
+
+
+// console.log("\n\n\n\n\n")
+
+// diasComS();
